25effdd13a586638be4e1b86beaac534bad18f11
M-kos <koc.melnikov.1985@gmail.com>
2019-07-06 22:32:30 +0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 import './style.css';
+
+const btn = document.querySelector(".refresh");
+
+
+const url = 'https://random.dog/woof.json';
+
+btn.addEventListener('click', e => {
+    const imgArr = document.querySelectorAll(".img-dog");
+
+    imgArr.forEach((el) => {
+        el.innerHTML = '';
+
+        el.addEventListener('click', e => {
+            e.preventDefault();
+            if (e.target.parentElement.classList.contains("large"))
+                return;
+
+            imgArr.forEach(el => {
+                if (el.classList.contains("large")) {
+                    el.classList.remove("large");
+                    el.classList.add("small");
+                }
+            });
+            e.target.parentElement.classList.add("large");
+        });
+        
+        getImg(url)
+            .then(json => { insertImg(el, json)} )
+            .catch();
+
+    });
+});
+
+function getImg(url) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                let json = JSON.parse(xhr.response);
+                resolve(json);
+            } else {
+                reject(xhr.statusText);
+            }
+        }
+        xhr.onerror = function(error) {
+            reject(error);
+        }
+        xhr.send();
+    }) 
+}
+
+function insertImg(elem, obj) {
+    let img = document.createElement('img');
+    img.setAttribute('src', obj.url);
+    elem.appendChild(img);
+}
